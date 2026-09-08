261b3b29c8cac6d64d875c0307af2554550f3ea7
Damith Dilanka <damithdilanka1996@gmail.com>
2026-09-08 10:59:34 +0530

    Add ApplicationDbContext and configure database connection in Program.cs

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using VertexCRM.Entities;
+
+namespace VertexCRM.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSets will be added here as entities are created
+    // Example: public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Entity configurations will be applied here
+    }
+}
